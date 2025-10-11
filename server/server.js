4cb9456788b51d78684b2cd863c205cd63e7d3e4
PranavRS18 <pranavsaravanan.r@gmail.com>
2025-10-11 23:52:55 +0530

    fix: change message

--- server/server.js
+++ server/server.js
@@ -128,7 +128,7 @@ app.post("/api/query", async (req, res) => {
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
+  console.log(`Server running on Port ${PORT}`);
 });
 
 
